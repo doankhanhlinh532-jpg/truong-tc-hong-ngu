022e2025d1a4f2888618e25bd0818e45d4d896e3
doankhanhlinh532-jpg <doankhanhlinh532@gmail.com>
2026-03-01 14:49:40 +0700

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,6 @@
+function ShowDog(){
+	document.getElementById("myImage").src="D:/thietkewed/anh/dog.jpg";
+}
+function ShowCat(){
+	document.getElementById("myImage").src="D:/thietkewed/anh/cat.jpg";
+}
